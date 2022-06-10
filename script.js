92686e875a543220b96ec9d9cce7b36060274af1
Actatus <itsactatus@yahoo.com>
2022-06-10 02:41:04 -0700

    Checks parantheses, passes 16/16 tests.

--- script.js
+++ script.js
@@ -40,14 +40,31 @@ function syntaxCheck(input){
   /*Check sytax, if there's a ( match with ), no more than 2 "-"*/
   let dashCheck = input.match(/-/g) ?? 0;
   let paranthesesCheck = input.match(/[()]/g) ?? 0;
+  let inputRegEx = input.match(/\d/g).join('');
+
   if (dashCheck.length > 2){
-    console.log("dashCheck " + input);
     return false;
   }
   if (paranthesesCheck !== 0 && paranthesesCheck.length !== 2){
-    console.log("paranthesesCheck " + input + " " + paranthesesCheck);
-    console.log(input + " " + paranthesesCheck.length);
     return false;
+  }
+  if (paranthesesCheck.length == 2){
+    if (inputRegEx.length == 10){
+      if (input[0] == "(" && input[4] == ")"){
+        return true;
+      } else {
+        return false;
+      }
+    } else if (inputRegEx.length == 11){
+      let inputMinusWhitespace = input.match(/\S/g);
+      if (inputMinusWhitespace[1] == "(" && inputMinusWhitespace[5] == ")"){
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return "Error, incorrect value";
+    }
   }
   return true;
 }
